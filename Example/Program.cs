1a34e3ceef98d4b01ada2359a783adf805f5a3d8
rebel <artyombel2003@gmail.com>
2024-04-15 21:06:43 +0300

    Example done

---
+++ Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using Core;
+
+namespace Example;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = new string[args.Length-1];
+        Array.Copy(args, 0, input, 0, input.Length);
+        var testsGenerator = new TestsGenerator();
+        
+        testsGenerator.Generate(input, args[args.Length-1]);
+    }
+}
